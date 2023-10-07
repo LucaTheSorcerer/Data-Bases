fe217dcd610a10a39375fa153a1557865dacb63d
LucaTheSorcerer <lucka.tudor@gmail.com>
2023-10-07 12:16:07 +0300

    it appears to be working

---
+++ Laborator/L1/employeeManagementSystem.sql
@@ -0,0 +1,11 @@
+USE employees;
+
+CREATE TABLE Employees (
+    EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Email NVARCHAR(100),
+    Phone NVARCHAR(20),
+    DepartmentID INT,
+
+);
