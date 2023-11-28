78575fa96b2aafc99968a76b915db440ce44c86a
LucaTheSorcerer <lucka.tudor@gmail.com>
2023-11-28 20:09:52 +0200

    L4 should be completed now

--- Laborator/L1/L4.sql
+++ Laborator/L1/L4.sql
@@ -187,60 +187,185 @@ END;
 
 SELECT * FROM Logger;
 DROP TABLE Logger;
-
+SELECT  * FROM Employees;
 INSERT INTO Employees (EmployeeID, FirstName, LastName, Email, Phone, DepartmentID, SalaryAmount, CurrentStatus)
     VALUES (18,'Ana','Dove','ana.dove@email.com','999-999-9999',2,50000.00,'Active');
 
 INSERT INTO Employees (EmployeeID, FirstName, LastName, Email, Phone, DepartmentID, SalaryAmount, CurrentStatus) VALUES
-    (19,'Maven','Johnson','maven.johnson@email.com','123-456-7890',1,60000.00,'Active'),
-    (20,'Bob','Dob','bob.dob@email.com','7890-456-123',1,65000.00,'Active');
+    (22,'Ion','Gheorghe','ion.gheorghe@email.com','123-456-7890',1,60000.00,'Active'),
+    (23,'BimBim','BamBam','bim.bam@email.com','7890-456-123',1,65000.00,'Active');
+
+
 
 
 UPDATE Employees
-SET SalaryAmount = 55000.00
+SET SalaryAmount = 5500.00
 WHERE EmployeeID = 18;
     SELECT @@ROWCOUNT AS AffectedRows;
 
+UPDATE Employees
+SET SalaryAmount = 6000.00
+WHERE EmployeeID IN (18, 22, 23);
+
+SELECT @@ROWCOUNT AS AffectedRows;
+
+
+
 DELETE FROM Employees
 WHERE EmployeeID = 18;
+
+DELETE FROM Employees
+WHERE EmployeeID IN (22, 23);
+SELECT @@ROWCOUNT AS AffectedRows;
+
+
 
 -- ================= END EXERCISE 3 =============
 
 -- ================= EXERCISE 4 =================
 
+-- CREATE TABLE UpdateLog (
+--     LogID INT PRIMARY KEY IDENTITY(1,1),
+--     EmployeeID INT,
+--     OldSalaryAmount DECIMAL(10, 2),
+--     NewSalaryAmount DECIMAL(10, 2),
+--     UpdateDate DATETIME
+-- );
+--
+-- CREATE OR ALTER PROCEDURE UpdateEmployeeSalary
+--     @EmployeeID INT,
+--     @NewSalaryAmount DECIMAL(10, 2)
+-- AS
+--     BEGIN
+--        DECLARE @OldSalaryAmount DECIMAL(10, 2);
+--
+--        SELECT @OldSalaryAmount = SalaryAmount
+--         FROM Employees
+--         WHERE EmployeeID = @EmployeeID;
+--
+--        UPDATE Employees
+--         SET SalaryAmount = @NewSalaryAmount
+--         WHERE EmployeeID = @EmployeeID;
+--
+--        INSERT INTO UpdateLog (EmployeeID, OldSalaryAmount, NewSalaryAmount, UpdateDate)
+--         VALUES (@EmployeeID, @OldSalaryAmount, @NewSalaryAmount, GETDATE());
+--     END;
+--
+--
+-- DECLARE EmployeeCursor CURSOR FOR
+-- SELECT EmployeeID, SalaryAmount
+-- FROM Employees;
+--
+-- DECLARE @EmployeeID INT, @OldSalaryAmount DECIMAL(10, 2), @NewSalaryAmount DECIMAL(10, 2);
+--
+-- OPEN EmployeeCursor;
+--
+-- FETCH NEXT FROM EmployeeCursor INTO @EmployeeID, @OldSalaryAmount;
+--
+-- WHILE @@FETCH_STATUS = 0
+-- BEGIN
+--     SET @NewSalaryAmount = ROUND(@OldSalaryAmount * 1.1, 2);
+--
+--     EXEC UpdateEmployeeSalary @EmployeeID, @NewSalaryAmount;
+--
+--     FETCH NEXT FROM EmployeeCursor INTO @EmployeeID, @OldSalaryAmount;
+-- END
+--
+-- CLOSE EmployeeCursor;
+-- DEALLOCATE EmployeeCursor;
+--
+--
+-- DROP TABLE UpdateLog;
+-- SELECT * FROM UpdateLog;
+-- SELECT * FROM Employees;
+
+-- ================= END EXERCISE 4 =============
+
+
+
+CREATE OR ALTER PROCEDURE UpdateAllEmployeeSalaries
+AS
+BEGIN
+    DECLARE @EmployeeID INT;
+    DECLARE @NewSalary DECIMAL(10, 2);
+
+    DECLARE EmployeeCursor CURSOR FOR
+    SELECT EmployeeID
+    FROM Employees;
+
+    OPEN EmployeeCursor;
+
+    FETCH NEXT FROM EmployeeCursor INTO @EmployeeID;
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        SET @NewSalary = ROUND((RAND() * (8800 - 5000) + 5000), 2);
+
+        UPDATE Employees
+        SET SalaryAmount = @NewSalary
+        WHERE EmployeeID = @EmployeeID;
+
+        FETCH NEXT FROM EmployeeCursor INTO @EmployeeID;
+    END;
+
+    CLOSE EmployeeCursor;
+    DEALLOCATE EmployeeCursor;
+END;
+
+
+EXEC UpdateAllEmployeeSalaries;
+
+DROP TABLE UpdateLog;
+
 CREATE TABLE UpdateLog (
     LogID INT PRIMARY KEY IDENTITY(1,1),
     EmployeeID INT,
     OldSalaryAmount DECIMAL(10, 2),
+    BonusAmount DECIMAL(10, 2),
+    BonusRating NVARCHAR(50),
     NewSalaryAmount DECIMAL(10, 2),
     UpdateDate DATETIME
 );
 
-CREATE OR ALTER PROCEDURE UpdateEmployeeSalary
+
+CREATE OR ALTER PROCEDURE dbo.AllocateBonusForEmployee
     @EmployeeID INT,
-    @NewSalaryAmount DECIMAL(10, 2)
+    @OldSalaryAmount DECIMAL(10, 2)
 AS
-    BEGIN
-       DECLARE @OldSalaryAmount DECIMAL(10, 2);
+BEGIN
+    DECLARE @BonusAmount DECIMAL(10, 2), @NewSalaryAmount DECIMAL(10, 2), @BonusRating NVARCHAR(50);
 
-       SELECT @OldSalaryAmount = SalaryAmount
-        FROM Employees
-        WHERE EmployeeID = @EmployeeID;
+    SET @BonusAmount = CASE
+                        WHEN (SELECT DepartmentID FROM Employees WHERE EmployeeID = @EmployeeID) = 1 THEN @OldSalaryAmount * 0.005
+                        WHEN (SELECT DepartmentID FROM Employees WHERE EmployeeID = @EmployeeID) = 2 THEN @OldSalaryAmount * 0.1
+                        WHEN (SELECT DepartmentID FROM Employees WHERE EmployeeID = @EmployeeID) = 3 THEN @OldSalaryAmount * 0.08
+                        ELSE 0.00
+                     END;
 
-       UPDATE Employees
-        SET SalaryAmount = @NewSalaryAmount
-        WHERE EmployeeID = @EmployeeID;
+    IF @BonusAmount > 500
+        SET @BonusRating = 'Excellent Performance';
+    ELSE IF @BonusAmount >= 250 AND @BonusAmount <= 500
+        SET @BonusRating = 'Good Performance';
+    ELSE
+        SET @BonusRating = 'Average Performance';
 
-       INSERT INTO UpdateLog (EmployeeID, OldSalaryAmount, NewSalaryAmount, UpdateDate)
-        VALUES (@EmployeeID, @OldSalaryAmount, @NewSalaryAmount, GETDATE());
-    END;
+    SET @NewSalaryAmount = @OldSalaryAmount + @BonusAmount;
+
+    UPDATE Employees
+    SET SalaryAmount = @NewSalaryAmount
+    WHERE EmployeeID = @EmployeeID;
+
+    INSERT INTO UpdateLog (EmployeeID, OldSalaryAmount, BonusAmount, BonusRating, NewSalaryAmount, UpdateDate)
+    VALUES (@EmployeeID, @OldSalaryAmount, @BonusAmount, @BonusRating, @NewSalaryAmount, GETDATE());
+END;
+
 
 
 DECLARE EmployeeCursor CURSOR FOR
 SELECT EmployeeID, SalaryAmount
 FROM Employees;
 
-DECLARE @EmployeeID INT, @OldSalaryAmount DECIMAL(10, 2), @NewSalaryAmount DECIMAL(10, 2);
+DECLARE @EmployeeID INT, @OldSalaryAmount DECIMAL(10, 2), @BonusAmount DECIMAL(10, 2), @NewSalaryAmount DECIMAL(10, 2);
 
 OPEN EmployeeCursor;
 
@@ -248,9 +373,7 @@ FETCH NEXT FROM EmployeeCursor INTO @EmployeeID, @OldSalaryAmount;
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-    SET @NewSalaryAmount = @OldSalaryAmount * 1.1;
-
-    EXEC UpdateEmployeeSalary @EmployeeID, @NewSalaryAmount;
+    EXEC dbo.AllocateBonusForEmployee @EmployeeID, @OldSalaryAmount;
 
     FETCH NEXT FROM EmployeeCursor INTO @EmployeeID, @OldSalaryAmount;
 END
@@ -260,4 +383,4 @@ DEALLOCATE EmployeeCursor;
 
 SELECT * FROM UpdateLog;
 
--- ================= END EXERCISE 4 =============
+SELECT * FROM Employees;
